a79566a5175cd6394b2c9748b559bc2646cfb1aa
ujin_bang <qkddnwls14@gmail.com>
2022-02-17 15:30:53 +0900

    방 데이터들을 추가

--- app/src/main/java/com/app40/jickbangcopy_20110217/MainActivity.kt
+++ app/src/main/java/com/app40/jickbangcopy_20110217/MainActivity.kt
@@ -2,10 +2,26 @@ package com.app40.jickbangcopy_20110217
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.app40.jickbangcopy_20110217.datas.RoomData
 
 class MainActivity : AppCompatActivity() {
+
+    val mRoomList = ArrayList<RoomData>()
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+
+        mRoomList.add(RoomData(8000,"서울시 동대문구",7,"1번째 방입니다"))
+        mRoomList.add(RoomData(19000,"서울시 서대문구",2,"2번째 방입니다"))
+        mRoomList.add(RoomData(15600,"서울시 동작구",3,"3번째 방입니다"))
+        mRoomList.add(RoomData(3800,"서울시 강남구",-1,"4번째 방입니다"))
+        mRoomList.add(RoomData(56000,"서울시 은평구",0,"5번째 방입니다"))
+        mRoomList.add(RoomData(6800,"서울시 성북구",-7,"6번째 방입니다"))
+        mRoomList.add(RoomData(34000,"경기도 고양시",15,"7번째 방입니다"))
+        mRoomList.add(RoomData(2000,"경기도 남양주시",0,"8번째 방입니다"))
+        mRoomList.add(RoomData(72000,"경기도 분당구",15,"9번째 방입니다"))
+
     }
 }
