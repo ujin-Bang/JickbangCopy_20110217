93f11a0a6122564baf9a6b10ea4e553eb2099504
ujin_bang <qkddnwls14@gmail.com>
2022-02-17 16:58:59 +0900

    넘겨준 방 데이터(Serializable 형태)를 RoomData로 원복  - 실제 데이터 반영

--- app/src/main/java/com/app40/jickbangcopy_20110217/ViewRoomDetailActivity.kt
+++ app/src/main/java/com/app40/jickbangcopy_20110217/ViewRoomDetailActivity.kt
@@ -2,6 +2,7 @@ package com.app40.jickbangcopy_20110217
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.app40.jickbangcopy_20110217.datas.RoomData
 import kotlinx.android.synthetic.main.activity_view_room_detail.*
 
 class ViewRoomDetailActivity : AppCompatActivity() {
@@ -9,7 +10,11 @@ class ViewRoomDetailActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_view_room_detail)
 
-        val price = intent.getStringExtra("priceInfo")
-        txtPrice.text = price
+        val roomData = intent.getSerializableExtra("roomInfo") as RoomData
+
+        txtPrice.text = roomData.getFormattedPrice()
+        txtDescription.text = roomData.description
+        txtAddress.text = roomData.address
+        txtFloor.text = roomData.getFormattedFloor()
     }
 }
